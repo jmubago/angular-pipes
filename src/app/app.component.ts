059bfaea84a5efd73c0a84d6e0228e2b23dff825
jmubago <jmubagosotomayor@gmail.com>
2019-03-10 13:54:12 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  public nombre: string = 'Jose';
+  public numeros: number[] = [1,2,3,4,5,6,7,8,9,10];
+  public pi: number = Math.PI;
+  public a:number = 0.2345;
+  public dinero:number = 1152.1567;
+  public heroe: any = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  }
+
+  public promesa: any = new Promise((resolve,reject) => {
+    setTimeout(() => resolve('los datos han llegado'),3500);
+  })
+
+  public fecha: Date = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+      provide: LOCALE_ID, useValue: 'es-CO'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
